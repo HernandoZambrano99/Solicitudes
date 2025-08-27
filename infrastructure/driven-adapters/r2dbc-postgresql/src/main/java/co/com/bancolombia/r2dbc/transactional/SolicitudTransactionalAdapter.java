c54e7ff8e8fdbb57e5fb823ebc428db0fe413b15
hernando <hernandozambrano556@gmail.com>
2025-08-27 11:33:22 -0500

    Se agrega transaccionalidad a la creación y busqueda de solicitudes.

---
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/bancolombia/r2dbc/transactional/SolicitudTransactionalAdapter.java
@@ -0,0 +1,27 @@
+package co.com.bancolombia.r2dbc.transactional;
+
+import co.com.bancolombia.model.SolicitudDetalle;
+import co.com.bancolombia.model.solicitud.Solicitud;
+import co.com.bancolombia.usecase.solicitud.SolicitudUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.reactive.TransactionalOperator;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class SolicitudTransactionalAdapter {
+
+    private final SolicitudUseCase solicitudUseCase;
+    private final TransactionalOperator transactionalOperator;
+
+    public Mono<SolicitudDetalle> crearSolicitud(Solicitud solicitud){
+        return solicitudUseCase.ejecutar(solicitud)
+                .as(transactionalOperator::transactional);
+    }
+
+    public Mono<Solicitud> buscarPorId(Integer id) {
+        return solicitudUseCase.buscarPorId(id)
+                .as(transactionalOperator::transactional);
+    }
+}
